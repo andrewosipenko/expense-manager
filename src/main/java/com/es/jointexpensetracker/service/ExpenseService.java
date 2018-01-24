038866909005cd2b31d06ac57edfff23c4fd00b4
Egor Poryadin <egor.pgg@gmail.com>
2018-01-24 14:11:07 +0300

    written Task 2.1

---
+++ src/main/java/com/es/jointexpensetracker/service/ExpenseService.java
@@ -0,0 +1,45 @@
+package com.es.jointexpensetracker.service;
+
+import com.es.jointexpensetracker.model.Expense;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class ExpenseService {
+    private static volatile ExpenseService instance = null;
+    private List<Expense> expenses;
+
+    private ExpenseService() {
+        String expenseGroup = UUID.randomUUID().toString();
+        expenses = Arrays.asList(
+                new Expense(1L, "Train tickets from Minsk to Warsaw", new BigDecimal(200), "Andrei", expenseGroup),
+                new Expense(2L, "Air tickets from Warsaw to Gran Carania and back", new BigDecimal(2000), "Ivan", expenseGroup),
+                new Expense(3L, "Restaurant", new BigDecimal(90), "Andrei", expenseGroup),
+                new Expense(4L, "Rent a car", new BigDecimal(700), "Sergei", expenseGroup),
+                new Expense(5L, "Rent a car", new BigDecimal(500), "Igor", expenseGroup),
+                new Expense(6L, "Rent a house", new BigDecimal(2000), "Igor", expenseGroup),
+                new Expense(7L, "Restaurant", new BigDecimal(60), "Andrei", expenseGroup),
+                new Expense(8L, "Gazoline", new BigDecimal(50), "Sergei", expenseGroup),
+                new Expense(9L, "Gazoline", new BigDecimal(50), "Igor", expenseGroup),
+                new Expense(10L, "Surfing", new BigDecimal(30), "Sergei", expenseGroup),
+                new Expense(11L, "New year party shopping", new BigDecimal(30), "Igor", expenseGroup),
+                new Expense(12L, "Surfing", new BigDecimal(30), "Sergei", expenseGroup),
+                new Expense(13L, "Air wing", new BigDecimal(50), "Sergei", expenseGroup),
+                new Expense(14L, "Bus tickets from Warsaw to Minsk", new BigDecimal(200), "Andrei", expenseGroup)
+        );
+    }
+
+    public static synchronized ExpenseService getService(){
+        if (instance != null)
+            return instance;
+        else
+            return instance = new ExpenseService();
+    }
+
+    public List<Expense> getExpenses(){
+        return expenses;
+    }
+
+}
--- src/main/java/com/es/jointexpensetracker/web/ExpenseListServlet.java
+++ src/main/java/com/es/jointexpensetracker/web/ExpenseListServlet.java
@@ -1,6 +1,7 @@
 package com.es.jointexpensetracker.web;
 
 import com.es.jointexpensetracker.model.Expense;
+import com.es.jointexpensetracker.service.ExpenseService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,28 +15,9 @@ import java.util.UUID;
 
 public class ExpenseListServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        String expenseGroup = UUID.randomUUID().toString();
-        request.setAttribute(
-            "expenses",
-            Arrays.asList(
-                new Expense(1L, "Train tickets from Minsk to Warsaw", new BigDecimal(200), "Andrei", expenseGroup),
-                new Expense(2L, "Air tickets from Warsaw to Gran Carania and back", new BigDecimal(2000), "Ivan", expenseGroup),
-                new Expense(3L, "Restaurant", new BigDecimal(90), "Andrei", expenseGroup),
-                new Expense(4L, "Rent a car", new BigDecimal(700), "Sergei", expenseGroup),
-                new Expense(5L, "Rent a car", new BigDecimal(500), "Igor", expenseGroup),
-                new Expense(6L, "Rent a house", new BigDecimal(2000), "Igor", expenseGroup),
-                new Expense(7L, "Restaurant", new BigDecimal(60), "Andrei", expenseGroup),
-                new Expense(8L, "Gazoline", new BigDecimal(50), "Sergei", expenseGroup),
-                new Expense(9L, "Gazoline", new BigDecimal(50), "Igor", expenseGroup),
-                new Expense(10L, "Surfing", new BigDecimal(30), "Sergei", expenseGroup),
-                new Expense(11L, "New year party shopping", new BigDecimal(30), "Igor", expenseGroup),
-                new Expense(12L, "Surfing", new BigDecimal(30), "Sergei", expenseGroup),
-                new Expense(13L, "Air wing", new BigDecimal(50), "Sergei", expenseGroup),
-                new Expense(14L, "Bus tickets from Warsaw to Minsk", new BigDecimal(200), "Andrei", expenseGroup)
-            )
-        );
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ExpenseService service = ExpenseService.getService();
+        request.setAttribute("expenses", service.getExpenses());
         request.getRequestDispatcher("WEB-INF/pages/expenses.jsp").forward(request, response);
     }
 }
