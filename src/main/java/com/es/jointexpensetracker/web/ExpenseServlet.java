17cb01d28711fc445b09bc44e74732c5ca88ce83
CameronCobb <cameroncoob@gmail.com>
2018-02-01 18:00:33 +0300

    Update ExpenseServlet.java

--- src/main/java/com/es/jointexpensetracker/web/ExpenseServlet.java
+++ src/main/java/com/es/jointexpensetracker/web/ExpenseServlet.java
@@ -34,7 +34,6 @@ public class ExpenseServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        System.out.println("Post");
         try
         {
             Long id = getExpenseID(request);
